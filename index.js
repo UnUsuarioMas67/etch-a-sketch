0c318535099e604eb3b048d251813a344076b671
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-08-14 20:21:23 -0400

    Move code to create grid into function

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const mainContainer = document.querySelector("#main-container");
+createGrid(16);
 
-// create 16x16 divs grid
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const div = document.createElement("div");
-    div.className = "square";
-    div.onmouseover = () => div.classList.add("hovered");
-    mainContainer.appendChild(div);
+function createGrid(gridSize) {
+  if (gridSize < 1 || gridSize > 100) {
+    throw new RangeError
   }
+
+  const containerWidth = getComputedStyle(mainContainer).getPropertyValue("width");
+  const squareSize = `calc(${containerWidth} / ${gridSize})`
+
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const div = document.createElement("div");
+      div.className = "square";
+      div.style.width = squareSize;
+      div.style.height = squareSize;
+      div.onmouseover = () => div.classList.add("hovered");
+
+      mainContainer.appendChild(div);
+    }
+  }   
 }
